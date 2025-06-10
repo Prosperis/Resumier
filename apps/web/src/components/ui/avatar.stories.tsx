fd79a8b886e4867e719178fcc7e47dc00e32eecc
Adrian Darian <adrian.the.hactus@gmail.com>
2025-06-10 02:56:59 -0700

    Add UI component stories (#98)

---
+++ apps/web/src/components/ui/avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+
+const meta: Meta<typeof Avatar> = {
+  title: "ui/Avatar",
+  component: Avatar,
+  tags: ["autodocs"],
+};
+export default meta;
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  render: () => (
+    <Avatar>
+      <AvatarImage src="https://i.pravatar.cc/300" alt="Avatar" />
+      <AvatarFallback>AV</AvatarFallback>
+    </Avatar>
+  ),
+};
